8aa9b2eeee8afdd2d7629e689733632abde88d32
Andrei Rotari <andreichina@gmail.com>
2023-08-23 04:49:37 -0700

    Insert, output data
    The reviews' form data is now inserted into database when the form is
    submitted.
    
    The reviews page accessible via a 'See reviews' button on the main page
    now displays actual data previously submitted by the 'Leave a review'
    form.

---
+++ source/php/output_reviews.php
@@ -0,0 +1,30 @@
+<?php
+
+$sql = "SELECT u.first_name, r.impression, r.details, r.id
+        FROM reviews r JOIN users_reviews ur ON r.id = ur.review_id JOIN users u ON ur.user_id = u.id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$context = [];
+foreach ($rows as $row) {
+    $firstName = $row['first_name'];
+    $impression = $row['impression'];
+    $details = $row['details'];
+
+    $sql = "SELECT s.name 
+            FROM sights s
+            JOIN sights_reviews sr ON s.id = sr.sight_id
+            JOIN reviews r ON sr.review_id = r.id
+            WHERE r.id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(1, $row['id']);
+    $stmt->execute();
+    $sights = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $sightNames = [];
+    foreach ($sights as $sight) {
+        $sightNames[] = $sight['name'];
+    }
+    $row['sights'] = $sightNames;
+    $context[] = $row;
+}
